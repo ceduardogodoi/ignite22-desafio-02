6c882d9964067f6077140ac9384093e9da8f9e10
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-08-17 11:20:47 -0300

    Adiciona fontes

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,4 +6,8 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  body, input, textarea, button {
+    font: 400 1rem 'Roboto', sans-serif;
+  }
 `
