1d98d61ece521707156ce6c5d547efe3f58720fb
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-08-26 16:47:26 -0300

    Finaliza página de Checkout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,131 @@
+import { MapPinLine, CurrencyDollar, CreditCard, Bank, Money, Trash } from 'phosphor-react'
+import espresso from '../../assets/images/coffe-types/espresso.png'
+import { QuantityCounter } from '../../components/Counter'
+import {
+  AddressLine1,
+  AddressLine2,
+  TextInput,
+  Card,
+  PaymentTypeButton,
+  MainContainer,
+  OrderSection,
+  SelectedCoffeesCard,
+  RemoveButton,
+  CoffeeListItem,
+  ActionsContainer,
+  Actions,
+  Summary,
+  PlaceOrderButton
+} from './styles'
+
+
 export function Checkout() {
-  return <h1>Checkout</h1>
+  return (
+    <MainContainer>
+      <OrderSection>
+        <h1>Complete seu pedido</h1>
+
+        <Card>
+          <header>
+            <div>
+              <MapPinLine color="#C47F17" size={22} />
+            </div>
+            <div>
+              <span>Endereço de entrega</span>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </header>
+
+          <form>
+            <TextInput type="text" placeholder="CEP" maxLength={8} />
+            <TextInput type="text" placeholder="Rua" />
+
+            <AddressLine1>
+              <TextInput type="text" placeholder="Número" />
+              <TextInput type="text" placeholder="Complemento" />
+            </AddressLine1>
+
+            <AddressLine2>
+              <TextInput type="text" placeholder="Bairro" />
+              <TextInput type="text" placeholder="Cidade" />
+              <TextInput type="text" placeholder="UF" maxLength={2} />
+            </AddressLine2>
+          </form>
+        </Card>
+
+        <Card>
+          <header>
+            <div>
+              <CurrencyDollar color="#8047F8" size={22} />
+            </div>
+            <div>
+              <span>Pagamento</span>
+              <p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
+            </div>
+          </header>
+
+          <PaymentTypeButton selected>
+            <CreditCard size={16} color="#8047F8" />
+            <span>Cartão de Crédito</span>
+          </PaymentTypeButton>
+          <PaymentTypeButton>
+            <Bank size={16} color="#8047F8" />
+            <span>Cartão de Débito</span>
+          </PaymentTypeButton>
+          <PaymentTypeButton>
+            <Money size={16} color="#8047F8" />
+            <span>Dinheiro</span>
+          </PaymentTypeButton>
+        </Card>
+      </OrderSection>
+
+      <OrderSection>
+        <h1>Cafés selecionados</h1>
+
+        <SelectedCoffeesCard>
+          <ul>
+            {new Array(2).fill(1).map(item => (
+              <CoffeeListItem key={item}>
+                <div>
+                  <img width={64} src={espresso} alt="" />
+                </div>
+
+                <ActionsContainer>
+                  <span>Expresso Tradicional</span>
+                  <Actions>
+                    <QuantityCounter />
+                    <RemoveButton>
+                      <Trash size={16} color="#8047F8" />
+                      <span>Remover</span>
+                    </RemoveButton>
+                  </Actions>
+                </ActionsContainer>
+
+                <strong>R$ 9,90</strong>
+              </CoffeeListItem>
+            ))}
+          </ul>
+
+          <Summary>
+            <div>
+              <span>Total de itens</span>
+              <span>R$ 29,70</span>
+            </div>
+
+            <div>
+              <span>Entrega</span>
+              <span>R$ 3,50</span>
+            </div>
+
+            <div>
+              <strong>Total</strong>
+              <strong>R$ 33,20</strong>
+            </div>
+          </Summary>
+
+          <PlaceOrderButton>Confirmar pedido</PlaceOrderButton>
+        </SelectedCoffeesCard>
+      </OrderSection>
+    </MainContainer>
+  )
 }
---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,241 @@
+import styled, { css } from 'styled-components'
+
+export const MainContainer = styled.main`
+  padding-top: 2.5rem;
+
+  display: flex;
+  column-gap: 2rem;
+`
+
+export const Card = styled.div`
+  padding: 2.5rem;
+  background: ${props => props.theme['base-card']};
+  border-radius: 6px;
+
+  & > header {
+    display: flex;
+    column-gap: 0.5rem;
+    margin-bottom: 2rem;
+
+    & :last-child {
+      display: flex;
+      flex-direction: column;
+
+      & > span {
+        font-size: 1rem;
+        margin-bottom: 0.125rem;
+        color: ${props => props.theme['base-subtitle']};
+      }
+
+      & > p {
+        font-size: 0.875rem;
+        color: ${props => props.theme['base-text']};
+      }
+    }
+  }
+
+  & > form {
+    display: grid;
+    gap: 1rem 0.75rem;
+
+    & [placeholder="CEP"],
+        [placeholder="Número"],
+        [placeholder="Bairro"] {
+      max-width: 12.5rem;
+    }
+
+    & [placeholder="Complemento"],
+      [placeholder="Cidade"] {
+      width: 100%;
+    }
+
+    & [placeholder="UF"] {
+      max-width: 3.75rem;
+    }
+  }
+
+  & + div {
+    margin-top: 0.75rem;
+  }
+`
+
+export const AddressLine1 = styled.div`
+  display: flex;
+  column-gap: 0.75rem;
+`
+
+export const AddressLine2 = styled.div`
+  display: flex;
+  column-gap: 0.75rem;
+`
+
+export const TextInput = styled.input`
+  padding: 12px;
+  background: ${props => props.theme['base-input']};
+  border: 1px solid ${props => props.theme['base-button']};
+  border-radius: 6px;
+  color: ${props => props.theme['base-text']};
+
+  &:focus {
+    outline: 1px solid ${props => props.theme['yellow-dark']};
+  }
+
+  &::placeholder {
+    font-size: 0.875rem;
+    color: ${props => props.theme['base-label']};
+  }
+`
+
+interface PaymentTypeButtonProps {
+  selected?: boolean
+}
+
+export const PaymentTypeButton = styled.button<PaymentTypeButtonProps>`
+  padding: 1rem 1.25rem;
+  border: none;
+  background: ${props => props.theme['base-button']};
+  color: ${props => props.theme['base-text']};
+  border-radius: 6px;
+  min-width: 11rem;
+  min-height: 3.125rem;
+
+  display: inline-flex;
+  align-items: center;
+
+  ${props => props.selected && css`
+    border: 1px solid ${props.theme['purple']};
+    background: ${props.theme['purple-light']};
+  `}
+
+  ${props => !props.selected && css`
+    &:hover {
+      background: ${props.theme['base-hover']};
+      color: ${props.theme['base-subtitle']};
+    }
+  `}
+
+  & > span {
+    margin-left: 0.75rem;
+    font-size: 0.75rem;
+    text-transform: uppercase;
+  }
+
+  & + button {
+    margin-left: 12px;
+  }
+`
+export const OrderSection = styled.section`
+  & > h1 {
+    margin-bottom: 1rem;
+  }
+
+  & + section {
+    flex: 1;
+  }
+`
+
+export const SelectedCoffeesCard = styled.div`
+  padding: 2.5rem;
+  background: ${props => props.theme['base-card']};
+  border-radius: 6px 44px;
+`
+
+export const RemoveButton = styled.button`
+  padding: 0.375rem 0.5rem;
+  border: none;
+  background: ${props => props.theme['base-button']};
+  border-radius: 6px;
+
+  display: flex;
+  align-items: center;
+  column-gap: 4px;
+
+  & > span {
+    font-size: 12px;
+    color: ${props => props.theme['base-text']};
+    text-transform: uppercase;
+  }
+
+  &:hover {
+    background: ${props => props.theme['base-hover']};
+    color: ${props => props.theme['base-subtitle']};
+  }
+`
+
+export const CoffeeListItem = styled.li`
+  padding-bottom: 1.5rem;
+  border-bottom: 1px solid ${props => props.theme['base-button']};
+
+  display: flex;
+
+  & > div + div {
+    margin-left: 20px;
+  }
+
+  & + li {
+    padding-top: 1.5rem;
+  }
+
+  & > strong {
+    margin-left: auto;
+    color: ${props => props.theme['base-text']};
+  }
+`
+
+export const ActionsContainer = styled.div`
+  & > span {
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${props => props.theme['base-subtitle']};
+  }
+`
+
+export const Actions = styled.div`
+  display: flex;
+  column-gap: 0.5rem;
+`
+
+export const Summary = styled.div`
+  padding: 1.5rem 0;
+
+  & div {
+    display: flex;
+    justify-content: space-between;
+
+    & > span {
+      font-size: 0.875rem;
+      color: ${props => props.theme['base-text']};
+
+      & + span {
+        font-size: 1rem;
+      }
+    }
+
+    & + div {
+      margin-top: 1rem;
+
+      & > strong {
+        color: ${props => props.theme['base-subtitle']};
+        font-size: 20px;
+      }
+    }
+  }
+
+`
+
+export const PlaceOrderButton = styled.button`
+  width: 100%;
+  min-height: 2.875rem;
+  padding: 0.75rem;
+  border: none;
+  border-radius: 6px;
+  background: ${props => props.theme.yellow};
+  color: ${props => props.theme.white};
+  text-transform: uppercase;
+  font-size: 0.875rem;
+  font-weight: 700;
+
+  &:hover {
+    background: ${props => props.theme['yellow-dark']};
+  }
+`
